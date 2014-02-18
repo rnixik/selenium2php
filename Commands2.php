83d8ed54b9e61a888427062e59777abaececb0a9
rNix <rnix@yandex.ru>
2014-02-18 16:32:30 +0700

    Fixed assertElementPresent and assertElementNotPresent

--- Commands2.php
+++ Commands2.php
@@ -165,8 +165,14 @@ class Commands2{
         $lines = array();
         $lines[] = 'try {';
         $lines[] = "    " . $this->_byQuery($target);
-        $lines[] = "    {$this->_obj}->assertTrue(true, true);";
-        $lines[] = '} catch (PHPUnit_Extensions_Selenium2TestCase_WebDriverException $e) {}';
+        $lines[] = "    {$this->_obj}->assertTrue(true);";
+        $lines[] = '} catch (PHPUnit_Extensions_Selenium2TestCase_WebDriverException $e) {';
+        $lines[] = "    if (PHPUnit_Extensions_Selenium2TestCase_WebDriverException::NoSuchElement === \$e->getCode()) {";
+        $lines[] = "        {$this->_obj}->assertTrue(false);";
+        $lines[] = "    } else { ";
+        $lines[] = "        throw \$e;";
+        $lines[] = "    }";
+        $lines[] = '}';
         return $lines;
     }
 
@@ -179,6 +185,7 @@ class Commands2{
         $lines = array();
         $lines[] = 'try {';
         $lines[] = "    " . $this->_byQuery($target);
+        $lines[] = "    {$this->_obj}->assertTrue(false);";
         $lines[] = '} catch (PHPUnit_Extensions_Selenium2TestCase_WebDriverException $e) {';
         $lines[] = "    {$this->_obj}->assertEquals(PHPUnit_Extensions_Selenium2TestCase_WebDriverException::NoSuchElement, \$e->getCode());";
         $lines[] = '}';
